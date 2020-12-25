40f26fc9f287cb631f19ddfd8232f5da4b133ac2
liuzhenghe30265 <15901450207@163.com>
2020-12-25 18:33:10 +0800

    fix:update

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import store from '../store/index.js'
 
+const LOGIN = true // 是否加登录功能
+
 Vue.use(Router)
 
 let routers = [{
@@ -44,40 +46,43 @@ let router = new Router({
   routes: routers,
 })
 const whiteList = ['/login'] // 不需要重定向的页
-router.beforeEach((to, form, next) => {
-  // to 即将要进入路由的对象
-  // from 要离开的路由对象
-  // 调用 next() 方法，进入下一个路由
-  let token = ''
-  try {
-    let userInfo = JSON.parse(sessionStorage.userInfo) // 将 sessionStorage 字符串解析为对象
-    // 防止刷新页面 vuex 中的用户信息清除，将 sessionStorage 中的用户信息再存储到 vuex 中
-    store.dispatch('setUserInfo', userInfo)
-    token = userInfo.token
-  } catch (error) {
-    token = ''
-  }
-  if (token) {
-    // 如果有 token，登录成功
-    if (to.path === '/login') {
-      // 访问登录页，就重定向到首页
-      next({
-        path: '/'
-      })
-    } else {
-      // 访问其他页面，进入下一个路由
-      next()
+
+if (LOGIN) {
+  router.beforeEach((to, form, next) => {
+    // to 即将要进入路由的对象
+    // from 要离开的路由对象
+    // 调用 next() 方法，进入下一个路由
+    let token = ''
+    try {
+      let userInfo = JSON.parse(sessionStorage.userInfo) // 将 sessionStorage 字符串解析为对象
+      // 防止刷新页面 vuex 中的用户信息清除，将 sessionStorage 中的用户信息再存储到 vuex 中
+      store.dispatch('setUserInfo', userInfo)
+      token = userInfo.token
+    } catch (error) {
+      token = ''
     }
-  } else {
-    // 没有 token
-    if (whiteList.indexOf(to.path) !== -1) {
-      // 在不需要重定向的页面直接登录
-      next()
+    if (token) {
+      // 如果有 token，登录成功
+      if (to.path === '/login') {
+        // 访问登录页，就重定向到首页
+        next({
+          path: '/'
+        })
+      } else {
+        // 访问其他页面，进入下一个路由
+        next()
+      }
     } else {
-      // 其他没有访问权限的页面被重定向到登录页
-      next(`/login?redirect=${to.path}`)
+      // 没有 token
+      if (whiteList.indexOf(to.path) !== -1) {
+        // 在不需要重定向的页面直接登录
+        next()
+      } else {
+        // 其他没有访问权限的页面被重定向到登录页
+        next(`/login?redirect=${to.path}`)
+      }
     }
-  }
-})
+  })
+}
 
 export default router
